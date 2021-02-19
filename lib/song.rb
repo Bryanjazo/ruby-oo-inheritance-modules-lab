5d9fde6d8e450e812565bb48929d37808aa79ba2
Bryanjazo <bryanjazo88@gmail.com>
2021-02-18 16:58:35 -0800

    commit tree 036f0e55ab9172293a03d415f282c0da0b33af3f

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 require 'pry'
-
 class Song
+
+  extend Findable::ClassMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
+
 
   @@songs = []
 
@@ -18,19 +24,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
 
-  def self.count
-    self.all.count
-  end
 
   def artist=(artist)
     @artist = artist
   end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
